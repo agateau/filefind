41ed87f0536a32cf01e20c39d2663b86d32e4ac9
Aurélien Gâteau <mail@agateau.com>
2016-12-06 09:33:29 +0100

    Make behavior closer to .gitignore, implement trailing **

--- stf/pattern.py
+++ stf/pattern.py
@@ -1,25 +1,59 @@
 from fnmatch import fnmatch
 
 
-class Pattern:
+SUPER_WILDCARD = '**'
+
+
+class PatternItem:
+    def match(self, text):
+        raise NotImplementedError
+
+    @staticmethod
+    def parse(pattern_text):
+        if pattern_text == SUPER_WILDCARD:
+            return SuperPatternItem()
+        else:
+            return PlainPatternItem(pattern_text)
+
+
+class PlainPatternItem(PatternItem):
     def __init__(self, pattern_text):
-        self._pattern_tokens = pattern_text.split('/')
+        self._pattern_text = pattern_text
+
+    def match(self, text):
+        return fnmatch(text, self._pattern_text)
+
+
+class SuperPatternItem(PatternItem):
+    def match(self, text):
+        return True
+
+
+class Pattern:
+    def __init__(self, text):
+        self._pattern_items = [PatternItem.parse(x) for x in text.split('/')]
 
     def match(self, path_components):
-        # Find match start
-        for idx, component in enumerate(path_components):
-            if fnmatch(component, self._pattern_tokens[0]):
-                break
+        pattern_items = self._pattern_items[:]
+        if len(self._pattern_items) == 1:
+            # name only pattern, find start
+            first = pattern_items.pop(0)
+            for idx, component in enumerate(path_components):
+                if first.match(component):
+                    break
+            else:
+                return False
         else:
-            return False
+            # multi path pattern, must match at start
+            idx = -1
 
         # Check rest of the pattern matches
-        for pattern in self._pattern_tokens[1:]:
+        for pattern in pattern_items:
             idx += 1
             if idx == len(path_components):
                 # We reached the end of the path, but we are not done with all
                 # the patterns
                 return False
-            if not fnmatch(path_components[idx], pattern):
+            if not pattern.match(path_components[idx]):
                 return False
         return True
--- tests/test_pattern.py
+++ tests/test_pattern.py
@@ -9,16 +9,20 @@ from stf.pattern import Pattern
     ('*.cpp', 'dir/foo.h', False),
 
     ('dir/*.cpp', 'dir/foo.cpp', True),
-    ('dir/*.cpp', 'sub/dir/foo.cpp', True),
+    ('dir/*.cpp', 'sub/dir/foo.cpp', False),
 
     ('dir', 'dir/foo.cpp', True),
     ('dir', 'sub/dir/foo.cpp', True),
     ('dir/foo', 'sub/dir/foo.cpp', False),
-    ('dir/foo', 'sub/dir/foo/bar.cpp', True),
+    ('dir/foo', 'sub/dir/foo/bar.cpp', False),
 
     ('sub/*/foo.cpp', 'sub/dir/foo.cpp', True),
     ('sub/*/foo.cpp', 'sub/dir1/dir2/foo.cpp', False),
-    ('sub/*/foo.cpp', 'root/sub/dir/foo.cpp', True),
+    ('sub/*/foo.cpp', 'root/sub/dir/foo.cpp', False),
+
+    # Trailing **
+    ('sub/**', 'sub/dir/foo.cpp', True),
+    ('sub/**', 'root/sub/dir/foo.cpp', False),
 ])
 def test_match(pattern_text, path, does_match):
     pattern = Pattern(pattern_text)

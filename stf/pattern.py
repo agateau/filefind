7d2e357a91ec788deee25c1c399f18945a93aae3
Aurélien Gâteau <mail@agateau.com>
2016-12-07 07:52:13 +0100

    Document

--- stf/pattern.py
+++ stf/pattern.py
@@ -5,6 +5,8 @@ SUPER_WILDCARD = '**'
 
 
 def find_pattern(pattern, path_components, start_idx=0):
+    """Returns the index of the first element of `path_components` starting at
+    `start_idx` to match `pattern`."""
     for idx, component in enumerate(path_components[start_idx:]):
         if fnmatch(component, pattern):
             return start_idx + idx
